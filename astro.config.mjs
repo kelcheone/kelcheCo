6742b76425aa2f58b691725fdcb477675bf5cd5f
kelche <kelcheone@gmail.com>
2023-01-05 20:28:10 +0300

    feat: google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { theme } from "./syntax-highlight-theme";
-import {
-  astroCodeSnippets,
-  codeSnippetAutoImport,
-} from "./integrations/astro-code-snippets";
+import { astroCodeSnippets, codeSnippetAutoImport } from "./integrations/astro-code-snippets";
 import AutoImport from "astro-auto-import";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 
@@ -24,21 +21,15 @@ import react from "@astrojs/react";
 import lit from "@astrojs/lit";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    AutoImport({
-      imports: [codeSnippetAutoImport],
-    }),
-    tailwind(),
-    sitemap(),
-    compress(),
-    astroCodeSnippets(),
-    mdx({
-      remarkPlugins: [remarkReadingTime],
-    }),
-    react(),
-    lit(),
-  ],
+  integrations: [AutoImport({
+    imports: [codeSnippetAutoImport]
+  }), tailwind(), sitemap(), compress(), astroCodeSnippets(), mdx({
+    remarkPlugins: [remarkReadingTime]
+  }), react(), lit(), partytown()],
   site: "https://www.kelche.co",
   base: "/",
   markdown: {
@@ -46,8 +37,8 @@ export default defineConfig({
     shikiConfig: {
       theme: "dracula",
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
+      wrap: true
+    }
   },
-  trailingSlash: "always",
+  trailingSlash: "always"
 });
