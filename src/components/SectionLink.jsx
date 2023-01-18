9bd0a605e3233b6900a8200418a5289916de492f
kelche <kelcheone@gmail.com>
2023-01-18 23:36:54 +0300

    feat: go bt-arr to string

---
+++ src/components/SectionLink.jsx
@@ -0,0 +1,19 @@
+// A component that can be used to share the a link to the current section of the page.
+
+import React from "react";
+
+import { AiOutlineLink } from "react-icons/ai";
+
+const SectionLink = ({ id, children }) => {
+  return (
+    <a
+      className="font-bold inline-block hover:underline"
+      href={`#${id}`}
+      aria-label={children}
+    >
+      <AiOutlineLink className="inline-block" />
+    </a>
+  );
+};
+
+export default SectionLink;
